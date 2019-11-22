07ca2c31dfea4c14903f158445c257ced6ab2338
Jakub Lebiedzinski <lebiedzinski.jakub.maciej@gmail.com>
2019-11-22 18:15:49 +0100

    PROJECT DONE

--- refactoring/src/app/card/card.component.ts
+++ refactoring/src/app/card/card.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  templateUrl: './card.component.html'
 })
 export class CardComponent implements OnInit {
 
