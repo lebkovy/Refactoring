ace4290aa0fab768ac37c5317484d22b2f421769
Jakub Lebiedzinski <lebiedzinski.jakub.maciej@gmail.com>
2019-11-22 17:08:29 +0100

    Ugly card almost done

--- refactoring/src/app/better-card/better-card.component.ts
+++ refactoring/src/app/better-card/better-card.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'better-card',
-  templateUrl: './better-card.component.html',
-  styleUrls: ['./better-card.component.scss']
+    selector: 'better-card',
+    templateUrl: './better-card.component.html',
+    styleUrls: ['./better-card.component.scss']
 })
-export class BetterCardComponent implements OnInit {
+export class BetterCardComponent {
+    discount = 20;
+    stars = this.returnArray(5);
+    rate = 3;
+    productTitle = 'Nike free run v2';
+    productSubtitle = 'Men Orangeblow Darken Sportswear Sneakers';
+    oldPrice = 178.47;
+    newPrice = 74.59;
+    sizes = [38, 39, 41, 42];
+    selectedSize = 39;
 
-  constructor() { }
+    returnArray(length: number): Array<any> {
+        return Array.from({ length }, (v, k) => k + 1);
+    }
 
-  ngOnInit() {
-  }
+    selectSize(size: number) {
+        this.selectedSize = size;
+    }
 
+    rateProduct(rate: number) {
+        this.rate = rate;
+    }
 }
